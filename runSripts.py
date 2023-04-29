db236db6f2b3cdfd40990f5142a74dff91c7ba1c
edwardghbaker <86253156+edwardghbaker@users.noreply.github.com>
2023-04-29 17:20:48 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   runSripts.py
    
    Changes not staged for commit:
    	modified:   BCF.py

--- runSripts.py
+++ runSripts.py
@@ -7,17 +7,8 @@ import glob
 #%% 
 
 
-HAL3 = r"C:\Users\r11403eb\OneDrive - The University of Manchester\SEM data\Edward Baker\SAH97072_position13_001_1.bcf"
-
-HAL3_image = images(HAL3,elements=['Fe'])
-maps = HAL3_image.makeMaps()
-
+img_dir = r"C:\Users\r11403eb\OneDrive - The University of Manchester\SEM data\Edward Baker\SAH97072_position13_001_11.bcf"
+img = images(img_dir,elements=['Fe','Mg','Si','Cl'],plot=True,save=True)
+maps = img.makeMaps()
 
 
-#%%
-fig, ax = plt.subplots(1,3)
-ax[0].imshow(HAL3_image.maps['Fe'])
-ax[1].imshow(HAL3_image.maps['Mg'])
-ax[2].imshow(HAL3_image.maps['Si'])
-
-# %%
